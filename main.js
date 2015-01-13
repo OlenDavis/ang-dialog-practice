3c4ae375091ec8cdcd9315e6ab4d5c1182573665
soutchay <chungvus@gmail.com>
2015-01-12 23:54:25 -0800

    fix styling of modal

--- main.js
+++ main.js
@@ -30,9 +30,10 @@ angular.module("myApp", [])
 		},
 		template:
 		"<div class='ng-modal' ng-show='show'>" +
+		//overlay will add a dim effect behind the modal
 			"<div class='ng-modal-overlay' ng-click='hideModal()'></div>" +
 			"<div class='ng-modal-dialog' ng-style='dialogStyle'>" +
-				"<div class='ng-modal-close' ng-click='hideModal()'>X</div>" +
+				"<div class='ng-modal-close' ng-click='hideModal()'></div>" +
 				"<div class='ng-modal-dialog-content' ng-transclude>" +
 			"</div>" +
 		"</div>"
