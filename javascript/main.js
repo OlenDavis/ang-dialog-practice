4ade862b252889f6878d71c86d04825336c39181
OlenDavis <OlenDavis@users.noreply.github.com>
2015-01-23 15:35:11 -0800

    notes

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,6 @@
 angular.module("myApp", [])
 .controller("MyController", ["$scope", function($scope){
+	// 1. all of these should have been attached to this rather than $scope so you can put this controller onto the page with ng-controller="MyController as MyController" and access them from things like {{ MyController.dialogContexts[ 0 ].title }}, which is more clear and even less prototypal-confusion-error-prone than attaching them all just to the $scope
 	$scope.dialogContexts = [{
 		title: 'A Dialog',
 		field: 'something'
@@ -27,6 +28,9 @@ angular.module("myApp", [])
 	$scope.clickDialog = function(selection){
 		$scope.selected = selection;
 	};
+	// It's fine to handle logic here like this, but it's also necessary to use logic like this to hide the things you can't do such as the next/previous buttons (rather than simply not doing them when you click on them).
+	// To that end, you might have implemented a method like this.canMoveRight = function( index ) { return index < this.dialogContexts.length - 1; } and then reused that in both a this.moveLeft( index ) { if ( this.canMoveLeft() ) ... } and in the template in an ng-if.
+	// All that said, this functionality (i.e. moving left and right and knowing which $index is the one that should be front and center) is the functionality the dialog-shelf directive was meant to encapsulate and make reusable all over; with this controller, you'd have to literally implement all that HTML and instantiate this controller with ng-controller each time you wanted a shelf/carousel of dialogs.
 	$scope.moveRight = function(selection){
 		if (selection > $scope.dialogContexts.length-2){
 			return;
@@ -60,6 +64,7 @@ angular.module("myApp", [])
 			};
 		},
 		template:
+		// ng-show is functional, but less performant, because if you had a lot of these, they'd all be fully rendered and sitting on the page (with all their $watch's being evaluated every digest loop), even when this ng-show evaluates to falsey. To do this performantly, you would need to use ng-if; but, if you use ng-if you wouldn't be able to use ng-transclude to transclude the dialog's contents because it would bind the transcluded contents to the dialog's isolate scope rather than the scope of wherever the dialog is used. And that's one of the two things that together make this particular programming challenge truly challenging. (The other of the two things that make this a true challenge is how do you make this directive such that it can be used anywhere inside an element with overflow: hidden, and not have that element crop the dialog and its backdrop.)
 		"<div class='ng-modal' ng-show='shown'>" +
 		//overlay will add a dim effect behind the modal
 			"<div class='ng-modal-overlay' ng-click='hideModal()'></div>" +
